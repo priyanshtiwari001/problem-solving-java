f2e121893e6cd9df9eb88f4390d135045092f684
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-01-25 09:34:29 +0530

    created star and number folder

---
+++ src/basic/pattern/starPatterns/Pattern8.java
@@ -0,0 +1,15 @@
+package basic.pattern.starPatterns;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Pushing to remote");
+
+
+
+
+    }
+}
